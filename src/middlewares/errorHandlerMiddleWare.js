fac888af2026c55be0b06ccec0d847696aa4e39e
Jeyhun <c.ashurbeyli@gmail.com>
2020-03-17 13:10:39 +0100

    Delete errorHandlerMiddleWare.js

--- src/middlewares/errorHandlerMiddleWare.js
+++
@@ -1,5 +0,0 @@
-import { INTERNAL_SERVER_ERROR } from 'http-status-codes';
-
-const errorHandler = (err, req, res, next) => err ? res.status(INTERNAL_SERVER_ERROR).send(err.stack) : next();
-
-export default errorHandler;
