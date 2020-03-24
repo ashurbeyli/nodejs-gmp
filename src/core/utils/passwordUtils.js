7e3581491332d30dd07983577d4ab60819236e7d
Jeyhun Ashurbayov <c.ashurbeyli@gmail.com>
2020-03-24 14:12:57 +0100

    refactor: verifyPassword method

--- src/core/utils/passwordUtils.js
+++ src/core/utils/passwordUtils.js
@@ -5,8 +5,10 @@ import { INVALID_PASSWORD } from '../../config/messages';
 import { JWT_SECRET_KEY } from '../../config/global';
 
 const hashPassword = (password) => passwordHash.generate(password);
-const verifyPassword = (password, hashedPassword) => new Promise((resolve, reject) => 
-    (passwordHash.verify(password, hashedPassword)) ? resolve(true) : reject({ message: INVALID_PASSWORD }));
+const verifyPassword = (password, hashedPassword) => {
+    if(!passwordHash.verify(password, hashedPassword)) throw { message: INVALID_PASSWORD };
+    return true;
+};
 const createToken = (data) => jwt.sign(data, JWT_SECRET_KEY);
 const verifyToken = (token) => jwt.verify(token, JWT_SECRET_KEY);
 
